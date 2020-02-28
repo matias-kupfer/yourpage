4e71c920033ca12d7a2e573d99dd8c55f7973b81
mmatiascfgm <matiascfgm@gmail.com>
2020-02-28 20:44:00 +0100

    minor changes on change profile picture component
    now showing upload progress bar and file size correctly

--- src/app/components/profile/change-picture/change-picture.component.ts
+++ src/app/components/profile/change-picture/change-picture.component.ts
@@ -25,7 +25,6 @@ export class ChangePictureComponent implements OnInit {
 
   updateProfileImage() {
     this.firestoreService.uploadImagesFirebase(this.files, this.userData);
-    this.dialogRef.close();
   }
 
   emptyFiles() {
